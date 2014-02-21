a6bb0195290240b9f4cc3c2c921697af9338f765
Denis K <f1nal@cgaming.org>
2014-02-21 17:25:19 +0400

    + brand new graphics

--- public/js/views/game.js
+++ public/js/views/game.js
@@ -21,13 +21,26 @@ define([
 			var min_angle = -35;
 			var canvasWidth = 320;
 			var canvasHeight = 640;
-			var rectWidth = 10;
-			var rectHeight = 10;
+			var rectWidth = 60;
+			var rectHeight = 112;
 			var ctx = $(this.el).find("#canvas").get(0).getContext("2d");
 			var debug = $(this.el).find("#debug");
 			
 			var x = (canvasWidth - rectWidth) / 2;
-			var y = 20;
+			var y = canvasHeight - rectHeight - 20;
+			
+			var spaceshipImage = new Image();
+			var backgroundImage = new Image();
+			var bossImage = new Image();
+			var stoneImage = new Image();
+			
+			spaceshipImage.src = "/images/spaceship.gif";
+			backgroundImage.src = "/images/background.jpg";
+			bossImage.src = "/images/boss.png";
+			stoneImage.src = "/images/stone.gif";
+			
+			var bossX = 50;
+			var bossRight = true;
 			
 			gyro.frequency = 10;
            	gyro.startTracking(function(o) {
@@ -55,8 +68,17 @@ define([
 				
 				ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 				
-				ctx.fillStyle = "#FF0000";
-				ctx.fillRect(x, y, rectWidth, rectHeight);
+				//ctx.fillStyle = "#FF0000";
+				//ctx.fillRect(x, y, rectWidth, rectHeight);
+				
+				ctx.drawImage(backgroundImage, 0, 0);
+				ctx.drawImage(spaceshipImage, x, y);
+				ctx.drawImage(bossImage, bossX, 20);
+				ctx.drawImage(stoneImage, 50, 200);
+				
+				bossX = (bossRight) ? bossX + 1 : bossX - 1;
+				
+				if (bossX > canvasWidth - 111 || bossX < 0) bossRight = !bossRight;
 			});
 			
             return this;
