96c2821d5e1978782e93a0a0ecb2aaeec893605c
Denis K <f1nal@cgaming.org>
2014-03-04 07:46:54 -0800

    + sexy background

--- public/js/models/gamecanvas.js
+++ public/js/models/gamecanvas.js
@@ -12,27 +12,31 @@ define([
 		backgroundImage: new Image(),
 		bossImage: new Image(),
 		stoneImage: new Image(),
+		backgroundMaxY: 696,
+		backgroundY: 0,
         initialize: function () {
 			this.spaceshipImage.src = "/images/spaceship.gif";
-			this.backgroundImage.src = "/images/background.jpg";
+			this.backgroundImage.src = "/images/background.png";
 			this.bossImage.src = "/images/boss.png";
 			this.stoneImage.src = "/images/stone.gif";
-			this.fps = 21;
+			this.fps = 60;
         },
         updateCanvas: function () {
             var ctx = $(".game__canvas").get(0).getContext("2d");
 
-			var canvasWidth = 320;
-			var canvasHeight = 640;
+			var canvasWidth = 1050;
+			var canvasHeight = 680;
 			var rectWidth = 60;
 			var rectHeight = 112;
 			        
             ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-			ctx.drawImage(this.backgroundImage, 0, 0);
+			ctx.drawImage(this.backgroundImage, 0, this.backgroundY, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight);
 			ctx.drawImage(this.spaceshipImage, gamelogic.playerX, gamelogic.playerY);
 			ctx.drawImage(this.bossImage, gamelogic.bossX, 20);
 			ctx.drawImage(this.stoneImage, 50, 200);
+
+			this.backgroundY = (this.backgroundY > 0) ? this.backgroundY - 1 : this.backgroundMaxY;
 
 			return this;
         },
