1d3c647279f6fae8b1488385961f189dcb2f3539
Denis K <f1nal@cgaming.org>
2014-03-18 07:43:59 -0700

    + gameover sync

--- public/js/views/gameover.js
+++ public/js/views/gameover.js
@@ -15,7 +15,7 @@ define([
     var View = Backbone.View.extend({
         template: tmpl,
         events: {
-            "click .score_submit__button": "submitScore"
+            "click .gameover__submit": "submitScore"
         },
         initialize: function () {
 			this.$el.hide();
@@ -26,28 +26,14 @@ define([
         },
         show: function() {
             this.$el.find(".score").text(gamelogic.scores);
-
 			this.$el.show();
         },
 		hide: function() {
 			this.$el.hide();
 		},
         submitScore: function() {
-/*
-            scores.add(new Score({ name: this.$el.find('.score_submit__name').val(), score: gamelogic.scores}));
-            scores.sync();
-*/
-            $.ajax({
-                url: "/scores",
-                type: "POST",
-                data: { name: this.$el.find('.score_submit__name').val(), score:gamelogic.scores }
-            }).done(function() {
-                //alert("success");
-            }).fail(function() { 
-                //alert("fail");
-            }).always(function() {
-                location.href = "#scoreboard";
-            });
+            scores.create({ name: this.$el.find('.gameover__name').val(), score: gamelogic.scores});
+            location.href = "#scoreboard";
         }
     });
 
